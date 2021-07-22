94ab2f8ebdcd48cc5c2a430a33ddb675a657b8b7
Jepson-Desktop <1102982941@qq.com>
2021-07-22 16:42:51 +0800

    发现了一个惊天大BUG，调制ZC的ofdm上进行频域共轭对称操作时应该从第2个点开始做对称！

--- draw_dis.m
+++ draw_dis.m
@@ -16,7 +16,7 @@ function draw_dis(cur_index)
             end
             hold(cfg.figure(1, 2),'off')
             xlim(cfg.figure(1, 2), [1, cfg.dislen]);
-            ylim(cfg.figure(1, 2), [1.1, 1.5]);
+            ylim(cfg.figure(1, 2), [1.3, 1.7]);
             for i=1:1:cfg.nin
                 if cfg.dis2(cur_index, i)~=-1
                 plot(cfg.figure(2, 2),[1:1:cfg.dislen],dis2(1+cur_index:cfg.dislen+cur_index, i),cfg.color(i));
