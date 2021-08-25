ae521d6eb6d3a57baf7c176dd5ec5cdf400c1ea7
Jepson-Desktop <1102982941@qq.com>
2021-08-25 16:56:28 +0800

    增加混淆矩阵

--- hmm.m
+++ hmm.m
@@ -1,62 +1,58 @@
 function hmm()
-O = 18*18;
-Q = 6;
+O = 18*18; % 观测状态
+Q = 6; % 分类数目
 
 % "true" parameters
 prior0 = normalise(rand(Q,1));
 transmat0 = mk_stochastic(rand(Q,Q));
 obsmat0 = mk_stochastic(rand(Q,O));
 
-% training data
-% T = 5;
-% nex = 10;
-% data = dhmm_sample(prior0, transmat0, obsmat0, T, nex);
-
 % initial guess of parameters
-prior0 = normalise(rand(Q,1));
-transmat0 = mk_stochastic(rand(Q,Q));
-obsmat0 = mk_stochastic(rand(Q,O));
+prior1 = normalise(rand(Q,1));
+transmat1 = mk_stochastic(rand(Q,Q));
+obsmat1 = mk_stochastic(rand(Q,O));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% training data
 data = load_dataset(1);
 % improve guess of parameters using EM
 [LL1, prior1, transmat1, obsmat1] = dhmm_em(data, prior0, transmat0, obsmat0, 'max_iter', 5);
-LL1
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 data = load_dataset(2);
 % improve guess of parameters using EM
 [LL2, prior2, transmat2, obsmat2] = dhmm_em(data, prior0, transmat0, obsmat0, 'max_iter', 5);
-LL2
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 data = load_dataset(3);
 % improve guess of parameters using EM
 [LL3, prior3, transmat3, obsmat3] = dhmm_em(data, prior0, transmat0, obsmat0, 'max_iter', 5);
-LL3
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 data = load_dataset(4);
 % improve guess of parameters using EM
 [LL4, prior4, transmat4, obsmat4] = dhmm_em(data, prior0, transmat0, obsmat0, 'max_iter', 5);
-LL4
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 data = load_dataset(5);
 % improve guess of parameters using EM
 [LL5, prior5, transmat5, obsmat5] = dhmm_em(data, prior0, transmat0, obsmat0, 'max_iter', 5);
-LL5
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 data = load_dataset(6);
 % improve guess of parameters using EM
 [LL6, prior6, transmat6, obsmat6] = dhmm_em(data, prior0, transmat0, obsmat0, 'max_iter', 5);
-LL6
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -77,7 +73,8 @@ LL6
     len = length(fileNames);
     num = 10000;
     cnt = 0;
-    
+    label_list = [];
+    pred_list = [];
     for i=1:1:num
         
         randi = unidrnd(len);
@@ -85,6 +82,9 @@ LL6
         fileName = fileName{1};
         sub_data = load([address, fileName]);
         sub_data = sub_data';
+        
+        label = str2num(fileName(1));
+        label_list = [label_list, label];
         
         % use model to compute log likelihood
         loglik1 = dhmm_logprob(sub_data, prior1, transmat1, obsmat1);
@@ -100,25 +100,23 @@ LL6
         
         [val, index] = sort(loglik);
         
-        y = index(6);
+        pred = index(6);
+        pred_list = [pred_list, pred];
         
-        
-        fprintf("数据编号："+fileName+" 分类结果：%d\n",y);
+%         fprintf("数据编号："+fileName(isstrprop(fileName,'digit'))+" 分类结果：%d\n",y);
 %         str2num(fileName(1))
-        if str2num(fileName(1)) == y
+        if label == pred
             cnt = cnt + 1;
+        else
+            fprintf("错误数据编号："+fileName(isstrprop(fileName,'digit'))+" 分类结果：%d\n",pred);
         end
         
     end
     
     
-        fprintf("acc: %f\n",cnt/num);
+    fprintf("acc: %f\n",cnt/num);
+    confusion_matrix1(label_list, pred_list)
     
-
-
-end
-
-function test()
 
 
 end
